4769a5bf85b00776663b45f3cfa7fa3a59b3c83c
Kirill <k.matviychuck@gmail.com>
2022-08-01 18:32:42 +0300

    header style refactored

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classNew from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import logoutImage from "../../assets/button/logout-pngrepo-com.png"
 
 type HeaderPropsType = {
     userLogin: string | null
@@ -9,20 +10,24 @@ type HeaderPropsType = {
 }
 
 const Header: React.FC<HeaderPropsType> = ({isAuth, userLogin, logoutUser}) => {
+    const logoutImg = {
+        backgroundImage: `url(${logoutImage})`,
+    }
     return (
         <header className={classNew.header}>
-            <div className={classNew.flexContainer}>
-                <div>
-                    <img src='https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'
-                         alt='headerImage'/>
+            <div className={classNew.headerContainer}>
+                <div className={classNew.leftSideContent}>
+                        <div>
+                            <img src='https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'
+                                 alt='headerImage'/>
+                        </div>
+                        <div>Social Network</div>
                 </div>
                 <div className={classNew.authWindow}>
                     <div className={classNew.login}>
                         {isAuth ? userLogin : <NavLink to={'/login'}>Login</NavLink>}
                     </div>
-                    <br/>
-                    {isAuth && <div className={classNew.logout} onClick={logoutUser}>
-                        Logout
+                    {isAuth && <div className={classNew.logout} onClick={logoutUser} style={logoutImg}>
                     </div>}
                 </div>
             </div>
